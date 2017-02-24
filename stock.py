8bc00c037373beaf9a6a3483389a3da78e5e4041
Dexter Lau <dexterlau@Dexters-MacBook-Pro.local>
2017-02-23 23:42:39 -0500

    added more code

--- stock.py
+++ stock.py
@@ -6,6 +6,10 @@ import matplotlib.style as style
 import matplotlib.dates as mdates
 from matplotlib.finance import candlestick_ohlc
 
+import bs4 as bs
+import pickle
+import requests
+
 def GetStockData(stock):
 
     today = dt.date.today()
@@ -17,20 +21,23 @@ def GetStockData(stock):
 
     return stock
 
-def PlotCandleStickGraph(stock, samples):
-    # Stock is a panda.dataframe and samples is the number of days to resample
+def PlotCandleStickGraph(ticker, dataframe, samples):
+    # ticker represents the stock ticker
+    # dataframe is a panda.dataframe and samples is the number of days to resample
+
     # make plots look pretty 
     style.use('ggplot')
 
+    # setup sample
     if (samples > 1):
         interval = str(samples) + 'd'
 
-        df_ohlc = stock['Adj Close'].resample(interval).ohlc()
-        df_volume = stock['Volume'].resample(interval).sum()
+        df_ohlc = dataframe['Adj Close'].resample(interval).ohlc()
+        df_volume = dataframe['Volume'].resample(interval).sum()
 
     elif (samples == 1):
-        df_ohlc = stock.drop('Volume', 1)
-        df_volume = stock['Volume']
+        df_ohlc = dataframe.drop('Volume', 1)
+        df_volume = dataframe['Volume']
 
     else:
         return
@@ -42,18 +49,40 @@ def PlotCandleStickGraph(stock, samples):
     df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
 
     # Setup plt
+    plt.figure(1)
+
     ax1 = plt.subplot2grid((7,1), (0,0), rowspan=5, colspan=1 )
     ax1.xaxis_date()
-    ax2 = plt.subplot2grid((7,1), (6,0), rowspan=1, colspan=1, sharex = ax1)
-
+    plt.title(ticker)
     candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
 
+    ax2 = plt.subplot2grid((7,1), (6,0), rowspan=1, colspan=1, sharex = ax1)
     ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
 
     plt.show()
 
     return 
 
-stock = GetStockData("AAPL")
+def save_sp500_ticker():
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'html.parser')
+    table = soup.find('table', {'class':'wikitable sortable'})
 
-PlotCandleStickGraph(stock, 1)
+    tickers = []
+
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker)
+
+    with open('sp500tickerlist.pickle', 'wb') as f:
+        pickle.dump(tickers,f)
+
+    return
+
+ticker = "AMD"
+
+# stock = GetStockData(ticker)
+
+# PlotCandleStickGraph(ticker, stock, 4)
+
+save_sp500_ticker()
